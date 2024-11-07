c6fedec2129d9b5c1b24c5fb0a6904a58ce570be
ggabaegii <117845415+ggabaegii@users.noreply.github.com>
2024-11-07 16:27:06 +0900

    flask 초기 설정

--- frontend/public/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker installing.');
-    // 캐시 또는 초기 설정 작업
-});
-
-self.addEventListener('fetch', (event) => {
-    console.log('Service Worker fetching.', event.request.url);
-    // 네트워크 요청을 처리하는 코드
-});
