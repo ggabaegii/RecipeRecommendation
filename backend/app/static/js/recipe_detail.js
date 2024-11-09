42ae79b126521b8beb05acc8c81826295c7ccd4d
rnjswnghl <rnjswnghl@gmail.com>
2024-11-10 00:15:32 +0900

    레시피 상세 화면 레시피를 보여주는 화면으로 틀 수정.세세하게 수정 필요. 임시완료

--- backend/app/static/js/recipe_detail.js
+++ backend/app/static/js/recipe_detail.js
@@ -1,38 +1,24 @@
 // 메뉴 슬라이드 기능
 function toggleMenu() {
     const menu = document.getElementById("menu");
-    if (menu.style.width === "250px") {
-        menu.style.width = "0";
-    } else {
-        menu.style.width = "250px";
-    }
+    menu.style.width = menu.style.width === "250px" ? "0" : "250px";
 }
 
-// 이미지 미리보기 기능
-function previewImage(event) {
-    const input = event.target;
-    const file = input.files[0];
+// 상호작용 아이콘 클릭 이벤트
+document.addEventListener("DOMContentLoaded", function() {
+    const likeIcon = document.querySelector(".icon.like");
+    const scrapIcon = document.querySelector(".icon.scrap");
+    const shareIcon = document.querySelector(".icon.share");
 
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const foodImage = document.getElementById('food-image');
-            foodImage.src = e.target.result;
-        };
-        reader.readAsDataURL(file);
-    }
-}
+    likeIcon.addEventListener("click", function() {
+        alert("이 레시피를 좋아요 했습니다!");
+    });
 
-// 재료 항목 추가 기능
-function addIngredientRow() {
-    const ingredientList = document.getElementById("ingredient-list");
-    const newRow = document.createElement("tr");
+    scrapIcon.addEventListener("click", function() {
+        alert("이 레시피를 스크랩했습니다!");
+    });
 
-    newRow.innerHTML = `
-        <td><input type="text" placeholder="재료명"></td>
-        <td><input type="text" placeholder="필요한 양"></td>
-        <td><input type="checkbox"></td>
-    `;
-
-    ingredientList.appendChild(newRow);
-}
+    shareIcon.addEventListener("click", function() {
+        alert("이 레시피 링크가 복사되었습니다!");
+    });
+});
